21bbccce548c6f5c30e59ed087f37b01e41bc42b
Luca Bracone <luca.bracone@elca.ch>
2024-10-08 18:23:12 +0200

    big changes to starship

---
+++ starship.nix
@@ -0,0 +1,78 @@
+{lib, ...}: {
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_status"
+        "$nix_shell"
+        "$aws"
+        "$azure"
+        "$gcloud"
+        "$line_break"
+        "$battery"
+        "$python"
+        "$character"
+      ];
+      right_format = lib.concatStrings ["$cmd_duration"];
+
+      directory.style = "bold green";
+      directory.read_only = " 󰌾";
+
+      character = {
+        success_symbol = "[❯](green)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](purple)";
+      };
+
+      battery.display = [
+        {
+          threshold = 10;
+          style = "bold red";
+        }
+        {
+          threshold = 30;
+          style = "bold yellow";
+        }
+      ];
+
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_state = {
+        format = "([$state( $progress_current/$progress_total)]($style)) ";
+        style = "bright-black";
+      };
+
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
+      };
+    };
+  };
+}
