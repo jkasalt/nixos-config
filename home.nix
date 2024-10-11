7f2a6e0bc6b56d8257bb0000d4b9264025fa9951
Luca Bracone <lucabr123@gmail.com>
2024-10-11 17:50:11 +0200

    add jaq

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
     vit
     lazygit
     fd
+    jaq
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
