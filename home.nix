938dbecb345c34089b51da1b5001501cb8813d24
Luca Bracone <lucabr123@gmail.com>
2024-11-28 11:14:42 +0100

    add ssh config

--- home.nix
+++ home.nix
@@ -19,7 +19,7 @@
     stateVersion = "24.05"; # Please read the comment before changing.
   };
 
-  imports = [./nixvim.nix ./zsh.nix ./openshift.nix];
+  imports = [./ssh.nix ./nixvim.nix ./zsh.nix ./openshift.nix];
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
---
+++ ssh.nix
@@ -0,0 +1,12 @@
+_: {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+      };
+    };
+  };
+}
