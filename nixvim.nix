6ce5fc3214ee5ecc3db94acea09ff2a1914575d3
Luca Bracone <lucabr123@gmail.com>
2024-12-03 15:14:50 +0100

    go back to none-ls

--- nixvim.nix
+++ nixvim.nix
@@ -56,75 +56,6 @@
         };
       };
 
-      conform-nvim = {
-        enable = true;
-        luaConfig.pre =
-          #Lua
-          "local slow_format_filetypes = {}";
-        settings = {
-          formatters_by_ft = let
-            shFormatters = ["shellcheck" "shellharden" "shfmt"];
-          in {
-            sh = shFormatters;
-            bash = shFormatters;
-            nix = ["alejandra"];
-            rust = ["rustfmt"];
-            "_" = [
-              "squeeze_blanks"
-              "trim_whitespace"
-              "trim_newlines"
-            ];
-          };
-          format_on_save =
-            # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                local function on_format(err)
-                  if err and err:match("timeout$") then
-                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                  end
-                end
-
-                return { timeout_ms = 200, lsp_fallback = true }, on_format
-               end
-            '';
-          format_after_save =
-            # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                return { lsp_fallback = true }
-              end
-            '';
-          log_level = "warn";
-          notify_on_error = false;
-          notify_no_formatters = false;
-          formatters = {
-            alejandra.command = lib.getExe pkgs.alejandra;
-            shellcheck.command = lib.getExe pkgs.shellcheck;
-            shfmt.command = lib.getExe pkgs.shfmt;
-            shellharden.command = lib.getExe pkgs.shellharden;
-            squeeze_blanks.command = lib.getExe' pkgs.coreutils "cat";
-            rustfmt.command = lib.getExe pkgs.rustfmt;
-          };
-        };
-      };
-
       dap.enable = true;
 
       fidget.enable = true;
@@ -189,6 +120,29 @@
             filetypes = ["yaml"];
           };
           nixd.enable = true;
+        };
+      };
+
+      lsp-format.enable = true;
+
+      none-ls = {
+        enable = true;
+        sources = {
+          code_actions = {
+            statix.enable = true;
+          };
+          diagnostics = {
+            actionlint.enable = true;
+            # sqruff.enable = true;
+            statix.enable = true;
+            tidy.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+            biome.enable = true;
+            # sqruff.enable = true;
+            typstyle.enable = true;
+          };
         };
       };
 
