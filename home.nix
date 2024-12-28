6d88965a21b9bca0f754d6b1c5ae5d85b52ba8e4
Luca Bracone <lucabr123@gmail.com>
2024-12-28 22:24:59 +0100

    added journal script

--- home.nix
+++ home.nix
@@ -67,6 +67,13 @@
       FILENAME="notes-$TODAY.md"
       nvim ~/notes/$FILENAME
     '')
+    (writeShellScriptBin "journal" ''
+      #!/usr/bin/env bash
+
+      TODAY=$(date -I)
+      FILENAME="$TODAY.md"
+      nvim ~/journal/$FILENAME
+    '')
     (writeShellScriptBin "nixcfg" ''
       #!/usr/bin/env bash
       cd ${config.home.homeDirectory}/nixos-config || exit 1 # how to make this more robust?
