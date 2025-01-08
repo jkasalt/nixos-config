09660eaafa09d6c7adc33839cd09299fb4823c26
Luca Bracone <lucabr123@gmail.com>
2025-01-08 09:43:07 +0100

    formatting

--- openshift.nix
+++ openshift.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}: let
   inherit (pkgs) openshift;
 in {
-  home.packages = [
-    openshift
-  ];
+  home.packages = [openshift];
 
-  programs.zsh.initExtra = ''
-    autoload -Uz compinit
-    compinit
-    if [ $commands[${openshift}/bin/oc] ]; then
-      source <(${openshift}/bin/oc completion zsh)
-      compdef _oc ${openshift}/bin/oc
-    fi
-  '';
+  programs.zsh.initExtra =
+    # bash
+    ''
+      autoload -Uz compinit
+      compinit
+      if [ $commands[${openshift}/bin/oc] ]; then
+        source <(${openshift}/bin/oc completion zsh)
+        compdef _oc ${openshift}/bin/oc
+      fi
+    '';
 }
