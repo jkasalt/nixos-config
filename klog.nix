3b0a017e42ef2f3e1a71cd7cee40975e4719856e
Luca Bracone <lucabr123@gmail.com>
2025-03-27 14:21:40 +0100

    change klog command

--- klog.nix
+++ klog.nix
@@ -2,7 +2,7 @@
   home.packages = [pkgs.klog-time-tracker];
   programs.zsh.shellAliases = {
     kle = "klog edit @work";
-    klc = "klog total --diff --now @work"; # like kl "count"
+    klc = "klog tags --today @work"; # like kl "count"
     klt = "klog today --diff --now @work";
   };
 }
